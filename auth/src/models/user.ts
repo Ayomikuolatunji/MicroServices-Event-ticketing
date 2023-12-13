68def012a2ae9e2e48d97b23c8afaef6d1e38919
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-12-13 16:23:49 +0100

    impr: change title to “Yield Amount (grams)“. The (grams)

---
+++ auth/src/models/user.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+interface IUser {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("User", userSchema);
